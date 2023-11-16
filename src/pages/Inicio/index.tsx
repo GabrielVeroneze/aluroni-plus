ff307ce70e72a56d0c624138710462edb15071f3
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2023-11-16 19:02:11 -0300

    :sparkles: feat: Cria página inicial

---
+++ src/pages/Inicio/index.tsx
@@ -0,0 +1,13 @@
+
+const Inicio = () => {
+    return (
+        <section>
+            <h3>Recomendações da cozinha</h3>
+            <div>
+                
+            </div>
+        </section>
+    )
+}
+
+export default Inicio
