69266bcbb4ab5d6f0039f57fda08bf7a3853e6c8
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2023-11-17 18:41:47 -0300

    :sparkles: feat: Utiliza componente de rotas

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import 'normalize.css'
 import '@/sass/_global.scss'
-import Cardapio from '@/pages/Cardapio'
+import AppRoutes from '@/routes'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <Cardapio />
+        <AppRoutes />
     </React.StrictMode>
 )
