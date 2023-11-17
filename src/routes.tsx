e34b0652db2485afe0a2ac208aa92131e75d2a9a
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2023-11-17 18:41:03 -0300

    :sparkles: feat: Cria componente para gerenciamento de rotas

---
+++ src/routes.tsx
@@ -0,0 +1,40 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { PratosProvider } from '@/context/Pratos/PratosContext'
+import { BuscaProvider } from '@/context/Busca/BuscaContext'
+import { FiltroProvider } from '@/context/Filtro/FiltroContext'
+import { OrdenarProvider } from '@/context/Ordenar/OrdenarContext'
+import Inicio from '@/pages/Inicio'
+import Cardapio from '@/pages/Cardapio'
+
+const AppRoutes = () => {
+    return (
+        <Router>
+            <Routes>
+                <Route
+                    path="/"
+                    element={
+                        <PratosProvider>
+                            <Inicio />
+                        </PratosProvider>
+                    }
+                />
+                <Route
+                    path="/cardapio"
+                    element={
+                        <PratosProvider>
+                            <BuscaProvider>
+                                <FiltroProvider>
+                                    <OrdenarProvider>
+                                        <Cardapio />
+                                    </OrdenarProvider>
+                                </FiltroProvider>
+                            </BuscaProvider>
+                        </PratosProvider>
+                    }
+                />
+            </Routes>
+        </Router>
+    )
+}
+
+export default AppRoutes
