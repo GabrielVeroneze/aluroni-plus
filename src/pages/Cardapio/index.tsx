baada843252de9c2d210a682dda8911c20efda53
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2023-11-18 16:08:44 -0300

    :fire: feat: Remove componentes

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -1,12 +1,6 @@
-import Cabecalho from '@/components/Cabecalho'
-import CardapioSecao from '@/components/CardapioSecao'
 
 const Cardapio = () => {
     return (
-        <main>
-            <Cabecalho />
-            <CardapioSecao />
-        </main>
     )
 }
 
