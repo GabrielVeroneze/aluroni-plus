9c808de2ac7957a752fafe74a02392dffef096bc
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2023-11-17 21:35:30 -0300

    :sparkles: feat: Renderiza os pratos recomendados dinâmicamente

--- src/pages/Inicio/index.tsx
+++ src/pages/Inicio/index.tsx
@@ -1,4 +1,5 @@
 import { usePratos } from '@/context/Pratos/usePratos'
+import Recomendado from '@/components/Recomendado'
 import styles from './Inicio.module.scss'
 
 const Inicio = () => {
@@ -11,9 +12,11 @@ const Inicio = () => {
 
     return (
         <section>
-                
             <h3 className={styles.titulo}>Recomendações da cozinha</h3>
             <div className={styles.recomendados}>
+                {pratosRecomendados.map(prato => (
+                    <Recomendado key={prato.id} prato={prato} />
+                ))}
             </div>
         </section>
     )
