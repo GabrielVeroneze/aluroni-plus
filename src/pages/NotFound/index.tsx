2c0e75c891a645ecdc90e5e3985edbe86ba94b9e
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2023-11-19 16:44:04 -0300

    :sparkles: feat: Adiciona botão de voltar
    Adiciona botão que volta para a página anterior acessada

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,9 +1,12 @@
+import { useNavigate } from 'react-router-dom'
 import classNames from 'classnames'
 import notFoundImagem from '@/assets/not_found.svg'
 import styles from './NotFound.module.scss'
 import stylesCommon from '@/sass/common/EstilosComuns.module.scss'
 
 const NotFound = () => {
+    const navigate = useNavigate()
+
     return (
         <section
             className={classNames({
@@ -11,6 +14,14 @@ const NotFound = () => {
                 [stylesCommon.container]: true,
             })}
         >
+            <div className={styles.voltar}>
+                <button
+                    className={styles.voltar__botao}
+                    onClick={() => navigate(-1)}
+                >
+                    &lt; Voltar
+                </button>
+            </div>
             <img src={notFoundImagem} alt="Erro 404" />
         </section>
     )
