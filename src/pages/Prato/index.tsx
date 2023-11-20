745d046481d2debebac38cbb84ac9ac6914baac3
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2023-11-19 23:46:06 -0300

    :fire: feat: Remove useLocation

--- src/pages/Prato/index.tsx
+++ src/pages/Prato/index.tsx
@@ -1,9 +1,6 @@
-import { useLocation } from 'react-router-dom'
 import styles from './Prato.module.scss'
 
 const Prato = () => {
-    const { state } = useLocation()
-    console.log(state)
     return (
         <div>Prato</div>
     )
