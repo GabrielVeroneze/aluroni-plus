6ef1110182a88844a48c802108e79e5c18508d7e
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2023-11-19 23:47:58 -0300

    :sparkles: feat: Adiciona o conteúdo da página

--- src/pages/Prato/index.tsx
+++ src/pages/Prato/index.tsx
@@ -1,8 +1,28 @@
+import Voltar from '@/components/Voltar'
+import Tags from '@/components/Tags'
 import styles from './Prato.module.scss'
 
 const Prato = () => {
     return (
-        <div>Prato</div>
+        <main className={styles.principal}>
+            <Voltar />
+            <section className={styles.prato}>
+                <h1 className={styles.prato__titulo}>
+                    {prato.title}
+                </h1>
+                <img
+                    className={styles.prato__imagem}
+                    src={prato.photo}
+                    alt={prato.title}
+                />
+                <div className={styles.prato__conteudo}>
+                    <p className={styles.prato__descricao}>
+                        {prato.description}
+                    </p>
+                    <Tags item={prato}/>
+                </div>
+            </section>
+        </main>
     )
 }
 
