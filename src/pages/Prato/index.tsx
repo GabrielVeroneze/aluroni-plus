f118b6d039256710d1e0cfad081622d7897494cf
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2023-11-20 16:12:53 -0300

    :sparkles: feat: Lida quando o prato não é encontrado
    Renderiza página 404 quando o prato não for encontrado

--- src/pages/Prato/index.tsx
+++ src/pages/Prato/index.tsx
@@ -1,4 +1,5 @@
 import { useEncontraPratoDaRota } from '@/hooks/useEncontraPratoDaRota'
+import NotFound from '@/pages/NotFound'
 import Voltar from '@/components/Voltar'
 import Tags from '@/components/Tags'
 import styles from './Prato.module.scss'
@@ -6,6 +7,10 @@ import styles from './Prato.module.scss'
 const Prato = () => {
     const { prato } = useEncontraPratoDaRota()
 
+    if (!prato) {
+        return <NotFound />
+    }
+    
     return (
         <main className={styles.principal}>
             <Voltar />
