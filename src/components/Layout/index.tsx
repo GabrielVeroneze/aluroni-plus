e5cdc4783ba5cf2c6e8fea60abb31afb060629c6
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2023-11-20 23:00:43 -0300

    :recycle: refactor: Modifica o Layout para receber children

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -2,12 +2,17 @@ import { Outlet } from 'react-router-dom'
 import Cabecalho from '@/components/Cabecalho'
 import stylesCommon from '@/sass/common/EstilosComuns.module.scss'
 
-const Layout = () => {
+interface LayoutProps {
+    children?: React.ReactNode
+}
+
+const Layout = ({ children }: LayoutProps) => {
     return (
         <main>
             <Cabecalho />
             <div className={stylesCommon.container}>
                 <Outlet />
+                {children}
             </div>
         </main>
     )
